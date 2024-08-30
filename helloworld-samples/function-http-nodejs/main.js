47137853e21071a37d2ff54280a86b294d0870de
sandrarapp <39089732+sandrarapp@users.noreply.github.com>
2024-08-30 12:27:13 +0200

    Require external package

--- helloworld-samples/function-http-nodejs/main.js
+++ helloworld-samples/function-http-nodejs/main.js
@@ -1,4 +1,10 @@
 module.exports.main = main = async (args) => {
+    // refernce external module 
+    const LoremIpsum = require("lorem-ipsum").LoremIpsum;
+    
+    // create a default text generator. 
+    const lorem = new LoremIpsum();
+    
     let url = 'https://httpbin.org/';
     requestData = {}
     if (args["__ce_method"] == "POST") {
